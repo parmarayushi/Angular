092f202c12c974b66593c596dd1d218fa7974bcc
Ayushi Parmar <ayushi.parmar@1Rintern.local>
2022-01-20 13:05:16 +0530

    initial

---
+++ myApp1/src/app/Test/child/child.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input('parentData') public name?:string;
+  @Output() public childEvent =new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.childEvent.emit("Hello...!");
+  }
+
+}
